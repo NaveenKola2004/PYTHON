e4bd982f9899caa63c7c5f94d9a91f7bb65bcf6f
NAVEEN <kolanaveen797@gmail.com>
2025-10-14 23:30:58 +0530

    added

--- java.java
+++ java.java
@@ -2,7 +2,7 @@ public class java{
     public static void main(String[] args) {
       String[] n={"naveen","apple","dog"};
       for (int i=0;i<n.length;i++){
-        System.out.println(n[i]);
+        System.out.println(i+1+" "+n[i]);
       }
     }
 }
